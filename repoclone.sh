7d5b49193a7d44c8f794fe30ed1e5c40466b017a
hguo <hguo@c2micro.com>
2011-07-28 18:28:58 +0800

    +repoclone.sh

---
+++ repoclone.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#goes to target folder and clone a repo from local, otherwise will from website.
+if [ ! -d .repo ]; then
+mkdir -p .repo;
+cd .repo;
+git clone ssh://git.bj.c2micro.com/c2sdk/repo.git;
+cd ..;
+fi
+
+BR=${1:-master}
+mf=${2:-default.xml}
+
+if [ -d .repo/manifests ]; then
+repo start $BR --all
+else
+yes "" | repo init -u ssh://git.bj.c2micro.com/c2sdk/manifests.git -b $BR -m $mf
+fi
+repo sync
+repo start $BR --all
